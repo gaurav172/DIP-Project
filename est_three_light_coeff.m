5f41303696e19e98ac92a4789b2159634cc61f1c
Ashit Rustagi <me.ashitrustagi@gmail.com>
2019-11-24 21:55:12 +0530

    another

--- est_three_light_coeff.m
+++ est_three_light_coeff.m
@@ -1,14 +1,3 @@
-% Code for Illuminant Spectra-based Source Separation Using Flash Photography
-% This code is based on the algorithm proposed in the paper
-% "Illuminant Spectra-based Source Separation Using Flash Photographye", CVPR 2018
-% Zhuo Hui, Kalyan Sunkavalli, Sunil Hadap, Aswin C. Sankaranarayanan
-% When you use the code to build your algorithm, please cite this paper. 
-% 
-% Please contact the author Zhuo Hui if you have any problems with the code
-% huizhuo1987@gmail.com
-% 
-% Copy rights reserved by the authors listed above.
-
 %% This function is to implement the esimtation of lighting coefficients
 
 % Input
